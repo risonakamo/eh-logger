f79de4c5c43f6e85d593a6dce7d7ea0260fe4e67
risonakamo <hmykanae@gmail.com>
2020-09-19 15:05:34 -0400

    button holding animation

--- popup/components/confirm-button/confirmbutton.tsx
+++ popup/components/confirm-button/confirmbutton.tsx
@@ -7,9 +7,15 @@ interface ConfirmButtonProps
   onClick():void //click event handler
 }
 
+interface ConfirmButtonState
+{
+  holding:boolean
+}
+
 export default class ConfirmButton extends React.Component
 {
   props:ConfirmButtonProps
+  state:ConfirmButtonState
 
   holdTimer:number
 
@@ -18,6 +24,10 @@ export default class ConfirmButton extends React.Component
     super(props);
     this.beginHoldTimer=this.beginHoldTimer.bind(this);
     this.endHoldTimer=this.endHoldTimer.bind(this);
+
+    this.state={
+      holding:false
+    };
   }
 
   beginHoldTimer():void
@@ -25,17 +35,23 @@ export default class ConfirmButton extends React.Component
     this.holdTimer=setTimeout(()=>{
       window.close();
     },2000);
+
+    this.setState({holding:true});
   }
 
   endHoldTimer():void
   {
     clearTimeout(this.holdTimer);
+    this.setState({holding:false});
   }
 
   render()
   {
+    var holdingClass:string=this.state.holding?"filling":"";
+
     return <div className="confirm-zone" onClick={this.props.onClick} onMouseDown={this.beginHoldTimer}
       onMouseUp={this.endHoldTimer} onMouseLeave={this.endHoldTimer}>
+      <div className={`fill-rect ${holdingClass}`}></div>
       <img src="/imgs/log-icon.png"/>
     </div>;
   }
