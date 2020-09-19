a4da47170cbac113e3391bd9ac2e98b234ebeb1a
risonakamo <hmykanae@gmail.com>
2020-09-19 00:18:02 -0400

    confirm button hold event

--- popup/components/confirm-button/confirmbutton.tsx
+++ popup/components/confirm-button/confirmbutton.tsx
@@ -11,9 +11,31 @@ export default class ConfirmButton extends React.Component
 {
   props:ConfirmButtonProps
 
+  holdTimer:number
+
+  constructor(props:ConfirmButtonProps)
+  {
+    super(props);
+    this.beginHoldTimer=this.beginHoldTimer.bind(this);
+    this.endHoldTimer=this.endHoldTimer.bind(this);
+  }
+
+  beginHoldTimer():void
+  {
+    this.holdTimer=setTimeout(()=>{
+      window.close();
+    },2000);
+  }
+
+  endHoldTimer():void
+  {
+    clearTimeout(this.holdTimer);
+  }
+
   render()
   {
-    return <div className="confirm-zone" onClick={this.props.onClick}>
+    return <div className="confirm-zone" onClick={this.props.onClick} onMouseDown={this.beginHoldTimer}
+      onMouseUp={this.endHoldTimer} onMouseLeave={this.endHoldTimer}>
       <img src="/imgs/log-icon.png"/>
     </div>;
   }
