45622ae196496d6af0eb9718010b148cc1db10f1
risonakamo <hmykanae@gmail.com>
2020-09-21 22:17:59 -0400

    trying some design

--- logviewer/components/logs-table/logstable.tsx
+++ logviewer/components/logs-table/logstable.tsx
@@ -6,7 +6,13 @@ export default function LogsTable():JSX.Element
 {
   return <table className="logs-table">
     <tbody>
-      <tr>
+      <tr className="NHENTAI">
+        <td className="date">09/21 00:36</td>
+        <td className="type">NH</td>
+        <td className="group">number2</td>
+        <td className="name">(C96) [Kabushikigaisha Toranoana (Various)] TORANOANA Girls Collection 2019 SUMMER TYPE-X</td>
+      </tr>
+      <tr className="NHENTAI">
         <td className="date">09/21 00:36</td>
         <td className="type">NH</td>
         <td className="group">number2</td>
