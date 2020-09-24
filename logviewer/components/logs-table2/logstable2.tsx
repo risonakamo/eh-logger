76f25106ed0c0c26cb5843cbf5262398895d47cc
risonakamo <hmykanae@gmail.com>
2020-09-24 00:11:25 -0400

    disable table horizontal scroll table header row

--- logviewer/components/logs-table2/logstable2.tsx
+++ logviewer/components/logs-table2/logstable2.tsx
@@ -12,6 +12,12 @@ interface LogsTableProps
 export default function LogsTable2(props:LogsTableProps):JSX.Element
 {
   return <div className="logs-table2">
+    <div className="log-row header-row">
+      <div className="log-col date">DATE</div>
+      <div className="log-col type">TYPE</div>
+      <div className="log-col group">GROUP</div>
+      <div className="log-col name">NAME</div>
+    </div>
     {props.logs.map((x:LogEntry,i:number)=>{
       return <LogRow2 entry={x} key={i}/>
     })}
