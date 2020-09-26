a62cb0a10b3fd6eed0eca14fe7ec14142fa8d9bb
risonakamo <hmykanae@gmail.com>
2020-09-25 22:58:12 -0400

    import button functionality

--- logviewer/components/import-button/import-button.tsx
+++ logviewer/components/import-button/import-button.tsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React,{useRef} from "react";
 
 import ColumnButton from "../column-button/column-button";
 
+import "./import-button.less";
+
 export default function ImportButton():JSX.Element
 {
-  return <ColumnButton text="Import Logs" icon="/imgs/export-icon.png"/>;
+  const fileInput=useRef<HTMLInputElement>(null);
+
+  // click handler. open file input.
+  function openFileInput(e:React.MouseEvent):void
+  {
+    e.preventDefault();
+    (fileInput.current as any).value=null;
+    fileInput.current!.click();
+  }
+
+  // file submitted handler.
+  async function fileSubmitted():Promise<void>
+  {
+    console.log(await readJsonFromFileInput<LogEntry[]>(fileInput.current!.files![0]));
+  }
+
+  return <>
+    <ColumnButton onClick={openFileInput} text="Import Logs" icon="/imgs/export-icon.png"/>
+    <input type="file" className="import-file-input" ref={fileInput} onChange={fileSubmitted}/>
+  </>;
+}
+
+// given a file object from a file input's files array, return a promise
+// with the json parsed readastext value
+function readJsonFromFileInput<T>(fileobject:File):Promise<T>
+{
+  return new Promise((resolve)=>{
+    var reader=new FileReader();
+    reader.onload=()=>{
+      resolve(JSON.parse(reader.result as string));
+    };
+
+    reader.readAsText(fileobject);
+  });
 }
