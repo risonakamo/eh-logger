ff1cf0f1d481b0906078118bdce7186c3a7a9c7c
risonakamo <hmykanae@gmail.com>
2020-11-18 23:46:57 -0500

    improve hitomi group name parse

--- pageparsers/hitomiparser.js
+++ pageparsers/hitomiparser.js
@@ -1,6 +1,13 @@
 (()=>{
+    var group=document.querySelector(".gallery h2").textContent.trim();
+
+    if (group=="N/A")
+    {
+        group=document.querySelector(".comma-list").textContent.trim()
+    }
+
     return {
         name:document.querySelector(".gallery h1").textContent,
-        group:document.querySelector(".gallery h2").textContent.trim()
+        group
     };
 })();
